09f9b47a42c56572407f8487016ecfc9ada6d7fb
Emil Strøm <Emil_hersvik_strom@hotmail.com>
2019-02-17 20:44:46 +0100

    Assignment uploaded.
    MA3 - Level 1 & 2.

--- AW-23 - Javascript 1/Module Assignment 3/level1/question1/script.js
+++ AW-23 - Javascript 1/Module Assignment 3/level1/question1/script.js
@@ -1,1 +1,15 @@
 //Use RegEx to validate form
+document.getElementById("submit").addEventListener("click", function(){
+    var input = document.getElementsByTagName("input");
+    
+    for(var i = 0; i < input.length; i++){
+        var inputs = input[i].value;
+        var pattern = new RegExp(input[i].pattern);
+        var test = pattern.test(inputs);
+        
+        if(test === false){
+            alert(input[i].name + " is invalid")
+        }
+    }
+})
+
--- AW-23 - Javascript 1/Module Assignment 3/level1/question2/script.js
+++ AW-23 - Javascript 1/Module Assignment 3/level1/question2/script.js
@@ -1,1 +1,18 @@
 //Console.log out elements in JSON file
+data = '[{"id" : 12312412312, "name" : "Ecuaciones Diferenciales", "url" : "/video/math/edo/12312412312"}]';
+
+author = '[{"name_author" : "Alejandro Morales", "url" : "/author/alejandro-morales", "type" : "master"}]';
+
+(function() {
+    var mydata = JSON.parse(data);
+    console.log(mydata[0].id);
+    console.log(mydata[0].name);
+    console.log(mydata[0].url);
+})();
+
+(function() {
+    var mydata = JSON.parse(author);
+    console.log(mydata[0].name_author);
+    console.log(mydata[0].url);
+    console.log(mydata[0].type);
+})();
